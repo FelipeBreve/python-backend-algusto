c25bf9fe6bb981c67ef048f36d298a832a0fb071
Felipe Breve <felipetedardi@gmail.com>
2018-05-17 23:52:31 -0300

    adicionado rota e template html

--- seminario-augusto/exemplo-python/primeiroprojeto/exemplo/views.py
+++ seminario-augusto/exemplo-python/primeiroprojeto/exemplo/views.py
@@ -2,5 +2,15 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+
+
+def home(request):
+    context = {'bem_vindo' : 'Bem vindo ao projeto de python Manitos'}
+    return render(request, 'home.html', context);
+
+# def home(request):
+#     data = {'name' : 'felipe', 'location': 'Brazil'}
+#     return JsonResponse(data, status=200)
--- seminario-augusto/exemplo-python/primeiroprojeto/primeiroprojeto/urls.py
+++ seminario-augusto/exemplo-python/primeiroprojeto/primeiroprojeto/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from exemplo import mensagem as mensagem
+from exemplo import views
 
-urlpatterns = [
+urlpatterns = [ 
     url(r'^admin/', admin.site.urls),
-    url(r'^$', mensagem.home, name='home')
+    url(r'^$', views.home, name='home')
 ]
