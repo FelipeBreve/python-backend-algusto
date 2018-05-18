a93e6f39b236e721390ef23a85c829b11b60c55f
Felipe Breve <felipetedardi@gmail.com>
2018-05-17 22:36:44 -0300

    atualizacao de urls

--- seminario-augusto/exemplo-python/primeiroprojeto/primeiroprojeto/urls.py
+++ seminario-augusto/exemplo-python/primeiroprojeto/primeiroprojeto/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from exemplo import mensagem as mensagem
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', mensagem.home, name='home')
 ]
