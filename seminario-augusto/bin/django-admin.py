5b336bdb5a69d4379cbaae3a55d6d6ba974cef00
Felipe Breve <felipetedardi@gmail.com>
2018-05-17 22:18:26 -0300

    projeto python init

--- seminario-augusto/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/felipe/Documentos/projetos-augusto/projeto-python/seminario-augusto/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
