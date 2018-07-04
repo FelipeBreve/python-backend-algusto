3056d2d2ec99e69067785cbe7954cd05467eca07
MatMercer <mateusmercer@gmail.com>
2018-07-04 15:54:18 -0300

    Add simple post form to home

--- src/exemplo/views.py
+++ src/exemplo/views.py
@@ -4,11 +4,24 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.utils.translation import gettext as _
+from exemplo.models import Msg
+from django.template import RequestContext
 
 # Create your views here.
 
-def home(request):
-    return render(request, 'home.html');
+def home(req):
+    if req.method == 'GET':
+        return render(req, 'home.html');
+    elif req.method == 'POST':
+        p = req.POST
+        author = p['author']
+        msg = p['msg']
+
+        if (author and msg):
+            # TODO: Continuar essa bagaça...
+            print("Fine!");
+
+        return render(req, 'home.html');
 
 # def home(request):
 #     data = {'name' : 'felipe', 'location': 'Brazil'}
